ac2faf0d4b553b9149913833efe904ef00801fe8
ngocphap <ngocphap5@gmail.com>
2021-07-09 05:32:16 +0700

    first commit

---
+++ GUI/Program.cs
@@ -0,0 +1,52 @@
+﻿using GUI.StaffWorking;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GUI
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            var baseDirectory = AppDomain.CurrentDomain.BaseDirectory;
+            string absolute = Path.GetFullPath(baseDirectory);
+            AppDomain.CurrentDomain.SetData("DataDirectory", absolute);
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Login loginForm = new Login();
+            DialogResult dr = loginForm.ShowDialog();
+            if (dr == DialogResult.OK)
+            {
+                if (checkPermission())
+                {
+                    Application.Run(new FeatureSelector());
+                }
+                else
+                {
+                    Application.Run(new TablesStatus());
+                }
+            }
+        }
+
+        private static bool checkPermission()
+        {
+            bool isManager = false;
+            foreach (DAL.EmployeeDepartment employeeDepartment in GlobalData.EMPLOYEE.EmployeeDepartments)
+            {
+                if (employeeDepartment.DepartmentID == 1)
+                    isManager = true;
+            }
+
+            return isManager;
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,33 @@
+
+# Restaurant Management
+
+## Migrate Database
+- Install SQLServer Express
+- Create a database with name "QLNhaHang"
+- In `DAL`, update `connectionStrings` in `App.config` (if necessary)
+- Go to View > Other Windows > Package Manager Console
+- At `Package Manager Console`, select default project is "DAL", run `Update-Database`
+
+## Login
+
+Admin
+
+	Username: admin
+	Password: admin
+
+Normal user
+
+	Username: user
+	Password: user
+## Main feature
+- Manage areas/tables
+- Manage menus
+- Manage employees
+- Create order (food, discount, vat, extra)
+- Cancel food
+- Change order's table
+- Merge tables
+- Print bill
+- Analytics
+- Export to excel file
+# Restaurant-POS-System-master
